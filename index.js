4e745bb5312010046b76316c07e4d785a7dfbc68
calm1205 <naginani072@gmail.com>
2024-06-02 18:44:29 +0900

    get company

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import { Builder, Browser } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome.js";
 import { kamei } from "./sites/kamei.js";
 
+const options = new chrome.Options();
+options.addArguments("--headless=new");
+
 (async () => {
-  const driver = await new Builder().forBrowser(Browser.CHROME).build();
+  const driver = await new Builder()
+    .forBrowser(Browser.CHROME)
+    .setChromeOptions(options)
+    .build();
+
   try {
     await kamei(driver);
+  } catch (error) {
+    console.error(error);
   } finally {
     await driver.quit();
   }
--- sites/kamei.js
+++ sites/kamei.js
@@ -5,13 +5,16 @@ const SITE_URL = "https://fc-kamei.net/BG1";
 export const kamei = async (driver) => {
   await driver.get(SITE_URL);
 
-  const companies = await driver.findElement(By.css(".brand_data_list"));
-  const companyInfo = companies.map((company) => {
-    const content = company.findElement(By.css("dd"));
-    const companyName = content[0].getText();
-    const companyAddress = content[3].getText();
-    return { companyName, companyAddress };
-  });
+  const companies = await driver.findElements(By.css(".brand_data_list"));
+
+  const companyInfo = await Promise.all(
+    companies.map(async (company) => {
+      const content = await company.findElements(By.css("dd"));
+      const companyName = await content[0].getText();
+      const companyAddress = await content[3].getText();
+      return { companyName, companyAddress };
+    })
+  );
 
   console.log(companyInfo);
 };
