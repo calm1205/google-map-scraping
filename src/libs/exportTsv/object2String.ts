c0ca496c3b41eea973abefcd27bc1e2bfbda30ff
calm1205 <naginani072@gmail.com>
2024-06-02 22:51:07 +0900

    header

--- src/libs/exportTsv/object2String.ts
+++ src/libs/exportTsv/object2String.ts
@@ -1,5 +1,12 @@
 import { TsvObject } from "./type";
 
+const headerObject: TsvObject[number] = {
+  name: "",
+  address: "",
+  phoneNumber: "",
+};
+const headerString = Object.keys(headerObject).join("\t") + "\n";
+
 /**
  * オブジェクトの配列をタブ区切り文字列に変換する
  */
@@ -8,10 +15,7 @@ export const object2String = (input: TsvObject) =>
     .map((object, index) => {
       const row = Object.values(object).join("\t");
 
-      if (index === 0) {
-        const header = Object.keys(object).join("\t") + "\n";
-        return header + row;
-      }
+      if (index === 0) return headerString + row;
 
       return row;
     })
