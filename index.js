9f6435eb912fe56f3bcce125cf0f1c3d8c3a02c7
calm1205 <naginani072@gmail.com>
2024-06-02 17:50:40 +0900

    set type module

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Builder, Browser } from "selenium-webdriver";
+
+(async () => {
+  const driver = await new Builder().forBrowser(Browser.CHROME).build();
+  try {
+    await driver.get("https://fc-kamei.net/BG1");
+
+    const title = await driver.findElement(
+      By.css("#list_header .brand_search_headline")
+    );
+
+    console.log(title);
+  } finally {
+    await driver.quit();
+  }
+})();
