c952016e612c380c7f6baa9c34392c73dc88163e
calm1205 <naginani072@gmail.com>
2024-06-08 08:48:06 +0900

    feat: copy index.html after transpile

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
+import { app, BrowserWindow } from "electron";
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  // トランスパイル後のpathを指定
+  window.loadFile("dist/index.html");
+};
+
+app.whenReady().then(createWindow);
+
 console.log("hello electron!");
