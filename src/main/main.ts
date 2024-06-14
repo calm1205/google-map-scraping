c3946ab9b9ecc48dfa0183e5d606fb765cf455e5
calm1205 <naginani072@gmail.com>
2024-06-14 18:05:17 +0900

    feat: electron main process

---
+++ src/main/main.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow, ipcMain } from "electron";
+import * as path from "node:path";
+import { fileURLToPath } from "url";
+import { googleMap } from "./puppeteer/googleMap.js";
+
+const createWindow = () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // トランスパイル後のpathを指定
+  window.loadFile("dist/index.html");
+};
+
+app.on("ready", () => {
+  ipcMain.handle("scraping", async () => await googleMap());
+  createWindow();
+});
+
+console.log("start electron!");
