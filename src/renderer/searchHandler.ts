0c210a8eb75a2355aeca6b71fbdb2402cff13d9d
calm1205 <naginani072@gmail.com>
2024-06-15 18:15:58 +0900

    fix: remove comment

--- src/renderer/searchHandler.ts
+++ src/renderer/searchHandler.ts
@@ -56,21 +56,12 @@ export const searchHandler: SearchHandler = {
     });
   },
 
-  /**
-   * ローディング中のステータスに変更
-   */
   _activateLoading() {
     if (this.dom.loader) this.dom.loader.style.display = "flex";
   },
-  /**
-   * ローディング中のステータスを解除
-   */
   _disableLoading() {
     if (this.dom.loader) this.dom.loader.style.display = "none";
   },
-  /**
-   * 検索中のステータスに変更
-   */
   _toSearchingStatus() {
     this.isSearching = true;
     this._activateLoading();
@@ -78,9 +69,6 @@ export const searchHandler: SearchHandler = {
     this._disableExport();
     this._activateStop();
   },
-  /**
-   * 検索後のステータスに変更
-   */
   _toSearchedStatus() {
     this.isSearching = false;
     this._activateExport();
@@ -88,67 +76,40 @@ export const searchHandler: SearchHandler = {
     this._disableLoading();
     this._disableStop();
   },
-  /**
-   * 検索ボタンの無効化
-   */
   _disableSearch() {
     if (this.dom.searchButton) this.dom.searchButton.disabled = true;
   },
-  /**
-   * スタートボタンの活性化
-   */
   _activateStart() {
     if (this.dom.searchButton) this.dom.searchButton.disabled = false;
   },
-  /**
-   * ストップボタンの活性化
-   */
   _activateStop() {
     if (this.isSearching) return;
     if (this.dom.stopButton) this.dom.stopButton.style.display = "block";
   },
-  /**
-   * エクスポートボタンの活性化
-   */
   _activateExport() {
     this.isExportable = true;
     if (this.dom.exportButton) this.dom.exportButton.style.display = "block";
   },
-  /**
-   * ストップボタンの無効化
-   */
   _disableStop() {
     if (this.dom.stopButton) this.dom.stopButton.style.display = "none";
   },
-  /**
-   * エクスポートボタンの無効化
-   */
   _disableExport() {
     this.isExportable = false;
     if (this.dom.exportButton) this.dom.exportButton.style.display = "none";
   },
 
-  /**
-   * 検索の中断
-   */
   stopSearch() {
     this.dom.stopButton?.addEventListener("click", () => {
       this._toSearchedStatus();
     });
   },
 
-  /**
-   * エクスポート
-   */
   export() {
     this.dom.exportButton?.addEventListener("click", () => {
       exportCsv(this.results);
     });
   },
 
-  /**
-   * スクレイピング
-   */
   async scraping({ keyword, maxCount }) {
     const companyInfo = await (window as any).electronAPI.scraping({
       keyword,
