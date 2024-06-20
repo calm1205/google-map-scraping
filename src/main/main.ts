b6548f58836f54cc3a5b12d0c545dee7f48d08b9
calm1205 <naginani072@gmail.com>
2024-06-20 09:37:32 +0900

    feat: install browser when app initialize

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,6 +2,7 @@ import { app, BrowserWindow, ipcMain } from "electron";
 import * as path from "node:path";
 import { fileURLToPath } from "url";
 import { scraping, ScrapingArgs } from "./sites/googleMap/index.js";
+import { installBrowser } from "./libs/installBrowser.js";
 
 export let mainWindow: BrowserWindow | null = null;
 
@@ -16,6 +17,8 @@ const createWindow = () => {
       preload: path.join(__dirname, "preload.js"),
     },
   });
+
+  installBrowser();
 
   // トランスパイル後のpathを指定
   mainWindow.loadFile("dist/index.html");
