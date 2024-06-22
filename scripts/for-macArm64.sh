e0e9e32f115a0c50c764206e029e7b951e0fd318
calm1205 <naginani072@gmail.com>
2024-06-22 14:55:04 +0900

    feat: build script for platform

--- scripts/for-macArm64.sh
+++ scripts/for-macArm64.sh
@@ -1,2 +1,7 @@
+# chromeのバイナリファイルをコピー
+cp -r chrome-mac-arm64 dist/chrome-mac-arm64
+
 # puppeteerの参照先のchromeのバイナリファイルをmacArm64用に変更する
 sed -i "" "s/executablePath: .*$/executablePath: path.join\(getRootPath\(\), CHROME_PATH.macArm64\),/g" dist/main/sites/googleMap/index.js
+
+echo "> Copied chrome-macArm64 to dist/ directory."
--- scripts/for-win64.sh
+++ scripts/for-win64.sh
@@ -1,2 +1,7 @@
+# chromeのバイナリファイルをコピー
+cp -r chrome-win64 dist/chrome-win64
+
 # puppeteerの参照先のchromeのバイナリファイルをwin64用に変更する
 sed -i "" "s/executablePath: .*$/executablePath: path.join\(getRootPath\(\), CHROME_PATH.win64\),/g" dist/main/sites/googleMap/index.js
+
+echo "> Copied chrome-win64 to dist/ directory."
