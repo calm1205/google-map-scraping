446d485dd4635eb469d4dcd4e4ff1da6d6adf0b9
calm1205 <naginani072@gmail.com>
2024-07-15 11:04:18 +0900

    feat: add zip script

---
+++ scripts/zip.cjs
@@ -0,0 +1,15 @@
+const zip = require("electron-installer-zip");
+
+zip(
+  {
+    dir: "./packed/web-scraping-darwin-arm64/web-scraping.app",
+    out: "./app", // output ./app.zip
+  },
+  (err, res) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log("Zip file written to: ", res);
+  }
+);
