74e8961022cb2c88deddb899a7981f8f11c706ec
calm1205 <naginani072@gmail.com>
2024-07-15 22:05:57 +0900

    fix: build script

--- scripts/for-win64.sh
+++ scripts/for-win64.sh
@@ -4,6 +4,6 @@ cp -iRa chromeBinaries/chrome-win64 dist/chrome-binary
 chrome_path="chrome-binary/chrome.exe"
 
 # puppeteerの参照先のchromeのバイナリファイルをwin64用に変更する
-sed -i "" "s/CHROME_PATH/$chrome_path/g" dist/main/sites/googleMap/index.js
+sed -i "" "s|CHROME_PATH|$chrome_path|g" dist/main/sites/googleMap/index.js
 
 echo "> Copied chrome-win64 to dist/ directory."
