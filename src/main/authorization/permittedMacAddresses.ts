e98d7e29ad7f2ef0eb7a9e2ed95a0d5cc83c8498
calm1205 <naginani072@gmail.com>
2024-07-17 20:08:12 +0900

    feat: add test user

--- src/main/authorization/permittedMacAddresses.ts
+++ src/main/authorization/permittedMacAddresses.ts
@@ -5,5 +5,6 @@ export const permittedMacAddresses = [
   "5c:e9:1e:b8:48:d2", // own
   "50:ed:3c:24:47:6b", // own2
   "4c:03:4f:a4:05:e7", // ida
+  "68:ca:c4:ac:46:c6", // test user1
   // "5c:e9:1e:b8:48:d0", // error
 ];
