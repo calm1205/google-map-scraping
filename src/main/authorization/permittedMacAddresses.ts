3e4914e7a325b055b6025ec20cb1ff490abdf419
calm1205 <authentic.nagi@gmail.com>
2024-07-26 21:31:05 +0900

    feat: add test user2

--- src/main/authorization/permittedMacAddresses.ts
+++ src/main/authorization/permittedMacAddresses.ts
@@ -5,7 +5,10 @@ export const permittedMacAddresses = [
   "5c:e9:1e:b8:48:d2", // own
   "50:ed:3c:24:47:6b", // own2
   "4c:03:4f:a4:05:e7", // ida
-  "68:ca:c4:ac:46:c6", // test user1
   "00:0d:3a:52:99:1d", // win
+
+  // test user
+  "68:ca:c4:ac:46:c6", // test user1
+  "58:ce:2a:de:b5:bb", // test user2
   // "5c:e9:1e:b8:48:d0", // error
 ];
