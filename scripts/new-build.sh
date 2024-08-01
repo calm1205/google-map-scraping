2600727a56a192c2481e198992d9770d12758c91
calm1205 <authentic.nagi@gmail.com>
2024-08-01 09:36:50 +0900

    fix: build後のファイルにpuppeteerを含める

--- scripts/new-build.sh
+++ scripts/new-build.sh
@@ -1,8 +1,18 @@
+# tailwindcssのビルド
 npm run tailwind
+
+# renderer(client side)のビルド
 npm run build
+
+# main(app)のビルド
 npm run transpile
+# main(app)のビルドにrendererも含まれるため、dist/rendererを削除
 rm -rf dist/renderer
 echo "> Removed dist/renderer directory."
 
-cp package.json dist/package.json
-echo "> Copied package.json to dist/ directory."
+# puppeteerのみを定義したpackage.jsonをdist/にコピー
+cp ./scripts/package.org.json dist/package.json
+echo "> Copied package.org.json to dist/ directory."
+
+cd dist
+npm install
